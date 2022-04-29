ea0588b28df265aaefd2e29ab880a314f65126ae
rogiave12 <63801689+rogiave12@users.noreply.github.com>
2022-04-29 09:05:22 +0200

    array is working, bugfix
    according to the selected box and X or Y turn it will add to the array X or Y. Checking for the score is needed to be done after this.

---
+++ Background.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Tic_Toe
+{
+    class Background
+    {
+        public Background(int playerX, int playerY, int tie, int timer)
+        {
+            PlayerX = playerX;
+            PlayerY = playerY;
+            Tie = tie;
+            Timer = timer;
+        }
+        public Background()
+        {
+            Observ = new ObservableCollection<Background>();
+        }
+
+        public int PlayerX { get; set; }
+        public int PlayerY { get; set; }
+        public int Tie { get; set; }
+        public int Timer { get; set; }
+
+        public ObservableCollection<Background> Observ{get;set;}
+
+        public void Adding()
+        {
+            Background b = new Background(PlayerX, PlayerY, Tie, Timer);
+            Observ.Add(b);
+        }
+        public void Checking(string[,] array)
+        {
+
+            //pomocí pomocného textboxu budu poznávat zda tam má být křížek nebo kolečko a pomocí datatrigeru budu na něj se odtaz, jestli tam je křížek nebo kolečko a podle toho poznám, jaký znak by tam měl být
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,99 @@ namespace Tic_Toe
     /// </summary>
     public partial class MainWindow : Window
     {
+        Background b = new Background();
+        string[,] arry = new string[3, 3];
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = b;
+            
+
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checking(object sender, RoutedEventArgs e)
+        {
+            RowChecking();
+           
+            Switching();
+            b.Checking(arry);
+
+        }
+        #region Switching X and Y
+        public void Switching()
+        {
+            if (turn.Text == "X turn")
+            {
+                turn.Text = "Y turn";
+            }
+            else if (turn.Text == "Y turn")
+            {
+                turn.Text = "X turn";
+            }
+        }
+        #endregion
+
+        #region Row checking
+        public void RowChecking()
+        {
+            char[] krizek = turn.Text.ToCharArray();//returns first letter of title
+
+            //if box is checked and if it has something then it will has a letter of the titile
+            if ((arrayColumn0Row0.IsChecked == true)&&((arry[0,0]==null)||(arry[0, 0] == "")))
+            {
+                arry[0, 0] = krizek[0].ToString();   
+            }
+            if (arrayColumn1Row0.IsChecked == true && ((arry[1, 0] == null) || (arry[1, 0] == "")))
+            {
+
+                
+                arry[1, 0] = krizek[0].ToString();
+            }
+            if (arrayColumn2Row0.IsChecked == true && ((arry[2, 0] == null) || (arry[2, 0] == "")))
+            {
+
+                
+                arry[2, 0] = krizek[0].ToString();
+            }
+            if (arrayColumn0Row1.IsChecked == true && ((arry[0, 1] == null) || (arry[0, 1] == "")))
+            {
+                
+                arry[0, 1] = krizek[0].ToString();
+            }
+            if (arrayColumn0Row2.IsChecked == true && ((arry[0, 2] == null) || (arry[0, 2] == "")))
+            {
+                
+                arry[0, 2] = krizek[0].ToString();
+            }
+
+            if (arrayColumn1Row1.IsChecked == true && ((arry[1, 1] == null) || (arry[1, 1] == "")))
+            {
+                
+                arry[1, 1] = krizek[0].ToString();
+            }
+            if (arrayColumn1Row2.IsChecked == true && ((arry[1, 2] == null) || (arry[1, 2] == "")))
+            {
+                
+                arry[1, 2] = krizek[0].ToString();
+            }
+            if (arrayColumn2Row1.IsChecked == true && ((arry[2, 1] == null) || (arry[2, 1] == "")))
+            {
+                
+                arry[2, 1] = krizek[0].ToString();
+            }
+            if (arrayColumn2Row2.IsChecked == true && ((arry[2, 2] == null) || (arry[2, 2] == "")))
+            {
+                
+                arry[2, 2] = krizek[0].ToString();
+            }
+
+
+        }
+        #endregion
     }
 }
